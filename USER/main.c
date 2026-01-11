6b5ed0a9b13933542a2ac34419d054c8fc609aa8
Leo <103794599@qq.com>
2026-01-11 18:44:12 +0800

    add dds

--- USER/main.c
+++ USER/main.c
@@ -7,6 +7,11 @@
 #include "ppm.h"
 
 uint8_t test_payload[] = {0xAA, 0x55, 0x12}; // 二进制: 10101010, 01010101, 00010010
+uint8_t  Wave_Type = AD9833_Out_Sinus;
+uint32_t Wave_Fre = 175000;
+float CH1_Phase = 0;	// 范围0~360
+float CH2_Phase = 0;	// 范围0~360
+
 
 int main(void)
 {
@@ -17,16 +22,23 @@ int main(void)
     delay_init(168);                // 初始化延时函数
     uart_init(115200);              // 初始化USART
     LED_Init();                     // 初始化LED
-    KEY_Init();                     // 初始化按键 (使用你提供的代码)
+    KEY_Init();                     // 初始化按键
+		AD9833_Init();								  // AD7606引脚初始化
     
+		AD9833_Set_Frequency(AD9833_ALL, Wave_Type, (uint32_t)(Wave_Fre * 10.73742));		// 发送频率控制字
+    AD9833_Set_Phase(Wave_Type, (uint32_t)(Wave_Fre * 10.73742), CH1_Phase, CH2_Phase);
+	
     PPM_Init();                     // 初始化PPM模块 (TIM3 Channel 1 @ PA6)
-
+	
     printf("STM32F407 PPM Generator Initialized.\r\n");
     printf("Pulse Width: 50us (10kHz equivalent)\r\n");
     printf("Press KEY0 to send data.\r\n");
+		
 
     while(1)
     {
+				
+
         key = KEY_Scan(0); // 扫描按键，不支持连按
         
         if(key == 1) // 你的代码逻辑里 KEY0 按下返回 KEY0_PRES (通常是1)
@@ -47,7 +59,8 @@ int main(void)
                 printf("System Busy.\r\n");
             }
         }
-        
-        delay_ms(10);
+        LED0 = !LED0;
+        delay_ms(100);
+
     }
 }
